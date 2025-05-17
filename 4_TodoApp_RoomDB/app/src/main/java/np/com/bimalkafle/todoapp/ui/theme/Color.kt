eea0de93ba33790197cd0d227176249fcf11fd6e
Abiy Arage <aragieabiy@gmail.com>
2025-05-18 00:24:58 +0300

    Todo App with kotlin and room database

---
+++ 4_TodoApp_RoomDB/app/src/main/java/np/com/bimalkafle/todoapp/ui/theme/Color.kt
@@ -0,0 +1,28 @@
+package np.com.bimalkafle.todoapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Light Theme Colors
+val PrimaryLight = Color(0xFF2196F3)      // Material Blue
+val SecondaryLight = Color(0xFF03A9F4)    // Light Blue
+val BackgroundLight = Color(0xFFF5F5F5)   // Light Gray Background
+val SurfaceLight = Color(0xFFFFFFFF)      // White
+val OnPrimaryLight = Color(0xFFFFFFFF)    // White
+val OnSecondaryLight = Color(0xFFFFFFFF)  // White
+val OnBackgroundLight = Color(0xFF000000) // Black
+val OnSurfaceLight = Color(0xFF000000)    // Black
+
+// Dark Theme Colors
+val PrimaryDark = Color(0xFF1976D2)       // Darker Blue
+val SecondaryDark = Color(0xFF0288D1)     // Darker Light Blue
+val BackgroundDark = Color(0xFF121212)    // Dark Background
+val SurfaceDark = Color(0xFF1E1E1E)       // Dark Surface
+val OnPrimaryDark = Color(0xFFFFFFFF)     // White
+val OnSecondaryDark = Color(0xFFFFFFFF)   // White
+val OnBackgroundDark = Color(0xFFFFFFFF)  // White
+val OnSurfaceDark = Color(0xFFFFFFFF)     // White
+
+// Accent Colors
+val AccentGreen = Color(0xFF4CAF50)       // Green for completed tasks
+val AccentRed = Color(0xFFF44336)         // Red for important tasks
+val AccentYellow = Color(0xFFFFC107)      // Yellow for pending tasks
